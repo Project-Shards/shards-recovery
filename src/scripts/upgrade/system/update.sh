6b2c1e8b2072b60484b20fecb8772cd8b8b28f8a
axtloss <axtlos@getcryst.al>
2023-04-05 13:29:38 +0200

    --noconfirmx :shipit:

--- src/scripts/upgrade/system/update.sh
+++ src/scripts/upgrade/system/update.sh
@@ -3,7 +3,7 @@ set -e # Make sure the script quits when a command fails
 set -x # Output the commands being ran
 
 echo "Upgrading System shard at /mnt/Shards/System"
-arch-chroot /mnt/Shards/System pacman -Syu --noconfirmx
+arch-chroot /mnt/Shards/System pacman -Syu --noconfirm
 
 echo "Unmounting /mnt/Shards/System/etc"
 umount /mnt/Shards/System/etc
