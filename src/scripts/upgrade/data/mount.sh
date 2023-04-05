314fe546b01c2c2c9ced91ad20b2161097b7d5fc
axtloss <axtlos@getcryst.al>
2023-04-05 13:52:36 +0200

    Unmount and remount Desktop shard manually instead of using remount

--- src/scripts/upgrade/data/mount.sh
+++ src/scripts/upgrade/data/mount.sh
@@ -2,8 +2,12 @@
 set -e # Make sure the script quits when a command fails
 set -x # Output the commands being ran
 
+ROOTDISK=$(df / | awk '{print $1}' | grep -v "Filesystem") # Get the root disk
+
 echo "Remount /mnt/Shards/Desktop as read-only"
-mount -o ro,remount /mnt/Shards/Desktop
+sync
+umount /mnt/Shards/Desktop
+mount -o ro "$ROOTDISK" /mnt/Shards/Desktop
 
 echo "Creating Overlay with System, Desktop and Data for /mnt/usr"
 mount -t overlay overlay -o lowerdir=/mnt/Shards/System/usr:/mnt/Shards/Desktop/usr,upperdir=/mnt/Shards/Data/usr,workdir=/mnt/Shards/Data/tmp/usr /mnt/usr
