417e459cb612bcc8266101df83dbfe079bcc3a1b
axtloss <axtlos@getcryst.al>
2023-04-05 13:54:19 +0200

    Add subvolume to desktop mount

--- src/scripts/upgrade/data/mount.sh
+++ src/scripts/upgrade/data/mount.sh
@@ -7,7 +7,7 @@ ROOTDISK=$(df / | awk '{print $1}' | grep -v "Filesystem") # Get the root disk
 echo "Remount /mnt/Shards/Desktop as read-only"
 sync
 umount /mnt/Shards/Desktop
-mount -o ro "$ROOTDISK" /mnt/Shards/Desktop
+mount -o ro,subvol=Desktop "$ROOTDISK" /mnt/Shards/Desktop
 
 echo "Creating Overlay with System, Desktop and Data for /mnt/usr"
 mount -t overlay overlay -o lowerdir=/mnt/Shards/System/usr:/mnt/Shards/Desktop/usr,upperdir=/mnt/Shards/Data/usr,workdir=/mnt/Shards/Data/tmp/usr /mnt/usr
