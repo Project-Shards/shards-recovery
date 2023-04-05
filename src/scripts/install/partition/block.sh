070321eb496cabe98bd7d725a37c4fd7a11fe716
axtloss <axtlos@getcryst.al>
2023-04-05 16:02:35 +0200

    remove early exit from script

--- src/scripts/install/partition/block.sh
+++ src/scripts/install/partition/block.sh
@@ -2,15 +2,11 @@
 set -e
 set -x
 
-sleep 2
-exit 0
-
 DISK=$1
 if [ -z "$DISK" ]; then
     echo "Usage: $0 <disk>"
     exit 1
 fi
-
 
 mkfs.vfat -F 32 -n "SHARDSEFI" ${DISK}1
 mkfs.btrfs -f -L "SHARDS" ${DISK}2
