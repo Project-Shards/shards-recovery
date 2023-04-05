49d6c7ce729dd42d58bea7e7680fc74081ac0dc4
axtloss <axtlos@getcryst.al>
2023-04-05 13:32:44 +0200

    Mount Shards at right directory

--- src/scripts/upgrade/data/mount.sh
+++ src/scripts/upgrade/data/mount.sh
@@ -6,10 +6,10 @@ echo "Remount /mnt/Shards/Desktop as read-only"
 mount -o ro,remount /mnt/Shards/Desktop
 
 echo "Creating Overlay with System, Desktop and Data for /mnt/usr"
-mount -t overlay overlay -o lowerdir=/Shards/System/usr:/Shards/Desktop/usr,upperdir=/Shards/Data/usr,workdir=/Shards/Data/tmp/usr /usr
+mount -t overlay overlay -o lowerdir=/Shards/System/usr:/Shards/Desktop/usr,upperdir=/Shards/Data/usr,workdir=/Shards/Data/tmp/usr /mnt/usr
 
 echo "Creating Overlay with System, Desktop and Data for /mnt/var"
-mount -t overlay overlay -o lowerdir=/Shards/System/var:/Shards/Desktop/var,upperdir=/Shards/Data/var,workdir=/Shards/Data/tmp/var /var
+mount -t overlay overlay -o lowerdir=/Shards/System/var:/Shards/Desktop/var,upperdir=/Shards/Data/var,workdir=/Shards/Data/tmp/var /mnt/var
 
 echo "Creating Overlay with System, Desktop and Data for /mnt/opt"
-mount -t overlay overlay -o lowerdir=/Shards/System/opt:/Shards/Desktop/opt,upperdir=/Shards/Data/opt,workdir=/Shards/Data/tmp/opt /opt
+mount -t overlay overlay -o lowerdir=/Shards/System/opt:/Shards/Desktop/opt,upperdir=/Shards/Data/opt,workdir=/Shards/Data/tmp/opt /mnt/opt
